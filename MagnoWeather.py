70e0a277befdea5a4f57fc1d3c41776944dd1305
harakan <wgt538@gmail.com>
2018-12-10 22:16:31 -0600

    Updated paths to work with current weather canada

--- MagnoWeather.py
+++ MagnoWeather.py
@@ -12,10 +12,13 @@ from urllib2 import urlopen
 #wget.download('http://www.google.com/url?q=http%3A%2F%2Fclimate.weather.gc.ca%2FclimateData%2Fbulkdata_e.html%3Fformat%3Dcsv%26stationID%3D42967%26Year%3D2014%26Month%3D1%26Day%3D1%26timeframe%3D2%26submit%3DDownload%2BData&sa=D&sntz=1&usg=AFQjCNEwIlyJ0esbczYOIhDB-mqKb2QiJg')
     #http://climate.weather.gc.ca/climateData/bulkdata_e.html?format={0}&stationID={1}&Year={2}&Month=1&Day=1&timeframe=2&submit=Download+Data
 
+
+#http://climate.weather.gc.ca/climate_data/bulk_data_e.html?hlyRange=2013-11-12%7C2018-12-09&dlyRange=2013-03-09%7C2018-12-09&mlyRange=%7C&StationID=51878&Prov=SK&urlExtension=_e.html&searchType=stnName&optLimit=yearRange&StartYear=1840&EndYear=2018&selRowPerPage=25&Line=2&searchMethod=contains&Month=10&Day=10&txtStationName=Prince+Albert&timeframe=2&Year=2018
+
 def getTemps(stationID,Year):
     try:
-        url="http://climate.weather.gc.ca/climateData/bulkdata_e.html?format=csv&stationID={0}&Year={1}&Month=1&Day=1&timeframe=2&submit=Download+Data".format(stationID,Year)
-        filename = wget.download(url)
+        url="http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID={0}&Year={1}&Month=1&Day=1&timeframe=2&submit=Download+Data".format(stationID,Year)
+        filename = wget.download(url, out="{0}_{1}.csv".format(stationID, Year))
         print "got File: {0}".format(filename)
     except:
         print "couldn't get url {0}".format(url)
@@ -23,7 +26,7 @@ def getTemps(stationID,Year):
 def getAllTemps(stationIDs=0):
     print "Starting Climate Downloader"
     fstation=open("canStation.csv")
-    for year in range(2014,2015): #Not actually 2015 though
+    for year in range(2014,2018): #Not actually 2015 though
         if stationIDs==0:
             for station in fstation:
                 getTemps(int(station),year)
@@ -39,17 +42,17 @@ def getCanStations():
 
     for pages in range(0,24):
         start=pages*100+1
-        website="http://climate.weather.gc.ca/advanceSearch/searchHistoricDataStations_e.html?searchType=stnProv&timeframe=1&lstProvince=&StartYear=1840&EndYear=2015&optLimit=specDate&Year=2000&Month=1&Day=1&selRowPerPage=100&cmdProvSubmit=Search&startRow={0}".format(start)
+        website="http://climate.weather.gc.ca/historical_data/search_historic_data_stations_e.html?searchType=stnProv&timeframe=1&lstProvince=&StartYear=1840&EndYear=2018&optLimit=specDate&Year=2018&Month=12&Day=5&selRowPerPage=100&txtCentralLatMin=0&txtCentralLatSec=0&txtCentralLongMin=0&txtCentralLongSec=0&startRow={0}".format(start)
         try:
             page = urlopen(website).read()
-            soup=BeautifulSoup(page, "html.parser") #lxml || html.parser
+            soup = BeautifulSoup(page, "html.parser") #lxml || html.parser
+            stations=soup.find_all(attrs={"name": "StationID"})
+            station=reID.findall(str(stations)) #regex out the station ID then store at the end of the station list
+            print "writing to file for {0}".format(pages)
+            for stationNum in station:
+                output.write("{0}\n".format(int(stationNum)))
         except:
             print "couldn't open page num: {0}".format(pages)
-        stations=soup.find_all(attrs={"name": "StationID"})
-        station=reID.findall(str(stations)) #regex out the station ID then store at the end of the station list
-        print "writing to file for {0}".format(pages)
-        for stationNum in station:
-            output.write("{0}\n".format(int(stationNum)))
 
 
 
